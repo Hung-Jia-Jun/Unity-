bc433434b8b350680c61498b750faa27c91d044c
JasonHome123 <yf9000555@gmail.com>
2015-12-02 20:01:51 +0800

    Update CameraControl.cs
    可以增加是否計算線性插值的相機

--- CameraControl.cs
+++ CameraControl.cs
@@ -1,34 +1,98 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraControl : MonoBehaviour {
-    private float X;//滑鼠X軸向量
-    private float Y;//滑鼠Y軸向量
-    public Transform target;//攝影機該對準的目標物件
-    public float Xspeed=200F;//X軸旋轉速度
-    public float Yspeed = 200;//Y軸旋轉速度
-    public float mSpeed = 10;//自身旋轉速度
-    public float yMinLimit =-50;//Y軸最小旋轉限制
-    public float yMaxLimit = 50;//Y軸最大旋轉限制
-    public Vector3 Angle;//角度
+public class CameraControl : MonoBehaviour
+{
+	
+
+    //相機跟隨的目標物件
+    public Transform MainCamera;
+    //觀察該物體的距離
+    public float Distance = 5F;
+    //旋轉速度
+    private float SpeedX = 240;
+    private float SpeedY = 120;
+    //角度限制
+    private float MinLimitY = 5;
+    private float MaxLimitY = 180;
+
+    //旋轉角度
+    private float mX = 0.0F;
+    private float mY = 0.0F;
+
+
+
+    //是否啟用差值
+    public bool isNeedDamping = true;
+    //速度
+    public float Damping = 10F;
+
+    private Quaternion mRotation;
+
     void Start()
     {
-        Angle = transform.eulerAngles;//獲取相機角度
-        X = Angle.x;//獲取相機角度的X軸
-        Y = Angle.y;//獲取相機角度的Y軸
+        //初始化旋轉角度
+        mX = transform.eulerAngles.x;
+        mY = transform.eulerAngles.y;
     }
-    void Update()
-    {
-        if (target) {
 
-            X += Input.GetAxis("Mouse X") * Xspeed * 0.02f;
-            Y -= Input.GetAxis("Mouse Y") * Yspeed * 0.02f;
-            Y = Mathf.Clamp(Y, yMinLimit, yMaxLimit);
-            if (X < -360) X = 0;
-            if (X > 360) X = 0;
-        }
+    void LateUpdate()
+    {
+     
+       
         
-      
+            //檢查滑鼠的X,Y軸輸入
+            mX += Input.GetAxis("Mouse X") * SpeedX * 0.02F;
+            mY -= Input.GetAxis("Mouse Y") * SpeedY * 0.02F;
+            //範圍限制
+           // mY = ClampAngle(mY, MinLimitY, MaxLimitY);
+            //計算旋轉
+            mRotation = Quaternion.Euler(mY, mX, 0);
+            //根據是否插值來采取不同的角度計算方式
+            if (isNeedDamping)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, mRotation, Time.deltaTime * Damping);
+            }
+            else
+            {
+                transform.rotation = mRotation;
+            }
+            
+        
+
+       
+
+        //重新計算位置
+        Vector3 mPosition = mRotation * new Vector3(0.0F, 0.0F, -Distance) + MainCamera.position;
+
+        //設定相機的角度與位置
+        if (isNeedDamping)
+        {
+            transform.position = Vector3.Lerp(transform.position, mPosition, Time.deltaTime * Damping);
+        }
+        else
+        {
+            transform.position = mPosition;
+        }
+
     }
-    
-}
+
+    //角度限制
+    private float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360) angle += 360;
+        if (angle > 360) angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+       
+	}
+
+
+
+
+   
+
+
+
+
+
