114c7f6a0873c5bcd34efb1e16e8db6f54a0ef68
JasonHome123 <yf9000555@gmail.com>
2015-12-27 14:02:20 +0800

    Create TextControl.cs

---
+++ TextControl.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TextControl : MonoBehaviour {
+	public GameObject ChefTextObj;//老闆Textbox
+	public GameObject CustomerTextObj;//顧客Textbox
+	public GameObject WaiterTextObj;//服務生Textbox
+	public int TalkSwitch;//說話狀態
+	// Use this for initialization
+	void Start () {
+
+		//TextObj.GetComponentInChildren<Text>().text="123";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (TalkSwitch)
+		{
+				case 0:
+				{
+
+				
+				
+				}
+		
+		
+		}
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+}
