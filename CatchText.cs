d6723383055776c755b1c8d9c0e9fbb88b22c485
JasonHome123 <yf9000555@gmail.com>
2016-02-11 15:03:04 +0800

    修改辨識條件
    並且增加可做txt文件寫入的程式碼
    可是後來發現不需要了
    所以就放在那邊等待呼叫

--- CatchText.cs
+++ CatchText.cs
@@ -1,22 +1,51 @@
 using UnityEngine;
 using System.Collections;
-
+using System.IO;
+using System.Collections.Generic;
+using System;
 public class CatchText : MonoBehaviour {
 	public TextAsset Atxt;//導入Txt
 	public string TxtRecognize;
-	void Start () 
+	public GameObject SceneControl;
+	public bool WriteMessage;//設定要寫入的訊息是否成功
+	void Start()
+	{
+		SceneControl=GameObject.Find("SceneControl");
+	}
+void FixedUpdate()
 	{
 		TxtRecognize = Atxt.text;//設定要辨識的字元來自於文字檔
 	}
-	public void CheckRecog(string RecoText)
+public void CheckRecog(string RecoText)//語音檢測
+{
+bool Status;
+if (RecoText == TxtRecognize)
 	{
-		if (RecoText==TxtRecognize)
-			{
-			Debug.Log("True");
-			}
-		else
-		{
-			Debug.Log ("false");
-		}
+		Status = true;
+		SceneControl.SendMessage ("ReturnCheck",Status);
 	}
+else
+    {
+		Status = false;
+		SceneControl.SendMessage ("ReturnCheck",Status);
+	}
+	
+}
+
+
+public void WriteFile(string info)
+   {
+   	  //文件流訊息
+	StreamWriter sw;
+	string fileLocal="D:\\unity\\Unity_5.1_Project\\ChineseRecognize_5.3-2\\Speech recognition5.3\\Assets\\A.txt";
+	FileInfo t = new FileInfo(fileLocal);
+	sw = t.AppendText();
+	//以行的形式寫入訊息
+	sw.WriteLine(info);
+	//關閉資料流
+	sw.Close();
+	//銷毀資料流
+	sw.Dispose();
+	WriteMessage=true;//已寫入資料
+   }  
 }
