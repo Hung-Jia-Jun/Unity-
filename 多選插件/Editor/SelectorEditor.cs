b1a21a5a29cdf1fb5f1680ad83416b0290c13b29
JasonHome123 <yf9000555@gmail.com>
2017-06-05 01:02:19 +0800

    Add files via upload
    可以多選的插件，放到unity 內
    然後把Selector.cs 掛載到遊戲物件內即可

---
+++ 多選插件/Editor/SelectorEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Selector))]
+public class SelectorEditor : Editor {
+	public override void OnInspectorGUI ( )
+	{
+		Selector selector = ( Selector )target;
+		DrawDefaultInspector();
+
+		EditorGUILayout.BeginHorizontal();
+		if ( GUILayout.Button("選擇以上物件") )
+		{
+			Selector.選擇場景內物件 ( selector.遊戲物件清單 );
+		}
+
+	}
+
+}
---
+++ 多選插件/Selector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Selector : MonoBehaviour
+{
+	public GameObject [ ] 遊戲物件清單;
+	public static void 選擇場景內物件 ( GameObject [ ] 遊戲物件清單 )
+	{
+		Selection.objects = 遊戲物件清單;
+	}
+
+}
