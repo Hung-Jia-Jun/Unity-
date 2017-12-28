8f2fdc7d6be1f6e4d58747ec842790609a7167c2
洪嘉駿 <yf9000555@gmail.com>
2017-12-29 03:12:04 +0800

    Fix when Touch the camera position is

--- TouchCamera.cs
+++ TouchCamera.cs
@@ -3,11 +3,12 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
-public class TouchCamera : MonoBehaviour {
+public class TouchCamera  : MonoBehaviour {
 	bool PCEditor;
 	void Start()
 	{
-		if (Application.platform == RuntimePlatform.OSXEditor) 
+		OldPos = transform.position;
+		if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.WindowsEditor) 
 		{
 			PCEditor = true;			
 		}
@@ -15,13 +16,26 @@ public class TouchCamera : MonoBehaviour {
 	}
 
 	public Vector3 OldPos;
-	public float dragSpeed=5;
+	public float dragSpeed=2;
 	private Vector3 dragOrigin;
-	//public InputField DragSpeed;
-	//public Text DragCount;
+	bool istouch;
+	public InputField DragSpeed;
+	public Text DragCount;
+	string ShowMsg;
+	Vector3 move ;
+	int TopIDZero;
+	Vector3 TempDist;
+	float ShowTempDist;
+	public Toggle UsePlan;
+	public void Reset()
+	{
+		transform.position = new Vector3 (0, 6, -10);
+		transform.eulerAngles = new Vector3(30,0,0);
+	}
 	void Update() {
+		
 		//dragSpeed = float.Parse(DragSpeed.text);
-		//DragCount.text = Input.touchCount.ToString ();
+
 		if (PCEditor) 
 		{
 			if (Input.GetMouseButtonDown(0))
@@ -45,27 +59,14 @@ public class TouchCamera : MonoBehaviour {
 
 		else 
 		{
-			if (Input.touchCount==2)
+			if (Input.touchCount > 1 && Input.GetTouch(0).phase == TouchPhase.Moved) 
 			{
-				dragOrigin = Input.GetTouch(0).position;
-				return;
+				Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+					transform.Translate (-touchDeltaPosition.x * 0.01f,0,-touchDeltaPosition.y * 0.01f);
+					transform.position=new Vector3(transform.position.x,6,transform.position.z);
+					DragCount.text = transform.position.ToString ();
+			
 			}
-
-			if (Input.touchCount == 0) //離開螢幕了
-			{
-				OldPos = transform.position;
-				return;
-
-			}
-			if (Input.touchCount == 2) 
-			{
-				Vector3 TouchNextPos = Input.GetTouch (0).position;
-				Vector3 pos = Camera.main.ScreenToViewportPoint(TouchNextPos - dragOrigin);
-				Vector3 move = new Vector3(OldPos.x-(pos.x*dragSpeed), OldPos.y,OldPos.z -(pos.y*dragSpeed));
-				transform.position = move;
-
-			}
-
 
 		}
 	}
